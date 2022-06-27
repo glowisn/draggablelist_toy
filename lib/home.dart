b9e5e6502e067622a188c566311426a0cb3ab42c
Yechan Jung <asdxcv1998@gmail.com>
2022-06-27 14:14:43 +0900

    [RLV] 0.1 Basic RLV example
    Basic flutter example from api flutter dev site.

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:draggablelist_toy/pages/reorderableListView.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Router'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Get.to(ReorderableListViewPage());
+              },
+              child: const Text("ReorderableListView"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
---
+++ lib/pages/reorderableListView.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ReorderableListViewPage extends StatefulWidget {
+  const ReorderableListViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ReorderableListViewPage> createState() =>
+      _ReorderableListViewPageState();
+}
+
+class _ReorderableListViewPageState extends State<ReorderableListViewPage> {
+  final List<int> _items = List<int>.generate(50, (int index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("RLV widget test page")),
+      body: ReorderableListView(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        children: <Widget>[
+          for (int index = 0; index < _items.length; index += 1)
+            ListTile(
+              key: Key('$index'),
+              tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+              title: Text('Item ${_items[index]}'),
+            ),
+        ],
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final int item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+      ),
+    );
+  }
+}
